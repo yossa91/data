43c03dd978e5bc4636c6e9ca9a9fe095aafbb440
yossa91 <y0ssa@naver.com>
2022-07-12 16:46:14 +0900

    0712

---
+++ day/vue0712/firstvue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <h1>뷰 처음 실행중입니다</h1>
+    <HelloWorld msg="임폴트와 프롬 익스폴트 개념 공부할것"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
